7ce2bf974fd5f53021d2a604149afbf350853a7b
Ondrej Richtr <on.richtr@seznam.cz>
2026-03-29 13:52:42 +0200

    bump version to v0.3

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ const String = [:0]const u8;
 // Public constants related to build configuration.
 // Please keep list of cpp and c files updated with all file names that need to get compiled.
 pub const project_name = "shooting_practice";
-pub const version_string = "v0.2";
+pub const version_string = "v0.3";
 
 pub const cpp_files = [_]String{ "collision.cpp", "drawing.cpp", "game.cpp", "lighting.cpp", "loop_data.cpp", "main-game.cpp", "main-menu.cpp",
                                  "main-test.cpp", "main.cpp", "meshes.cpp", "mouse_manager.cpp", "movement.cpp", "shaders.cpp",
